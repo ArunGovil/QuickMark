b439566e64ffeca079dc2cb04bca783dbeb61f87
ArunGovil <a.arungovil@gmail.com>
2022-11-20 15:18:47 +0530

    first commit

---
+++ source/screens/BoardingScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function BoardingScreen() {
+  return (
+    <View>
+      <Text>BoardingScreen</Text>
+    </View>
+  );
+}
